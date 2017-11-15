12bed80ec5bd912b6710fa72bddabcea8ca96e4c
Leo Yan <leo.yan@linaro.org>
2017-11-15 11:04:48 +0800

    Fix up csdump #1
    Signed-off-by: Leo Yan <leo.yan@linaro.org>

--- extensions/csdump.c
+++ extensions/csdump.c
@@ -21,11 +21,10 @@
 
 #define koffset(struct, member) struct##_##member##_offset
 
-
 /* at = ((struct *)ptr)->member */
 #define read_value(at, ptr, struct, member)				\
 	do {								\
-		readmem(ptr + koffset(struct, member), KVADDR,	\
+		readmem(ptr + koffset(struct, member), KVADDR,		\
 				&at, sizeof(at), #struct "'s " #member,	\
 				RETURN_ON_ERROR);			\
 	} while (0)
@@ -41,10 +40,14 @@
 		}							\
 	} while (0)
 
-static struct kernel_list_head *coresight_dump_list;
-
 static int koffset(coresight_dump_node, cpu);
 static int koffset(coresight_dump_node, list);
+static int koffset(coresight_dump_node, buf);
+static int koffset(coresight_dump_node, buf_size);
+static int koffset(coresight_dump_node, name);
+
+static struct kernel_list_head *coresight_dump_list;
+
 static int instance_count;
 
 int csdump_metadata(void)
@@ -53,10 +56,23 @@ int csdump_metadata(void)
 	struct list_data list_data;
 	struct list_data *ld = &list_data;
 	int i;
+	FILE *out_fp;
+	int ret;
+
+	init_offset(coresight_dump_node, cpu);
+	init_offset(coresight_dump_node, list);
+	init_offset(coresight_dump_node, buf);
+	init_offset(coresight_dump_node, buf_size);
+	init_offset(coresight_dump_node, name);
+
+	if ((out_fp = fopen("./metadata.bin", "w")) == NULL) {
+		fprintf(fp, "Cannot open file\n");
+		return FALSE;
+	}
 
 	sym_dump_list = symbol_search("coresight_dump_list");
 
-	/* Get pointer to struct pt */
+	/* Get pointer to dump list */
 	if (sym_dump_list == NULL) {
 		fprintf(fp, "symbol coresight_dump_list is not found\n");
 		return FALSE;
@@ -77,17 +93,17 @@ int csdump_metadata(void)
 	if (instance_count <= 0)
 		return 0;
 
-
-	init_offset(coresight_dump_node, cpu);
-	init_offset(coresight_dump_node, list);
-
 	/* We start i at 1 to skip over the list_head and continue to the last
 	 * instance, which lies at index instance_count */
 	for (i = 1; i <= instance_count; i++)
 	{
 		ulong instance_ptr;
 		ulong field;
-		int cpu;
+		ulong name_addr;
+		ulong buf_addr;
+		int cpu, buf_sz;
+		char name[64];
+		char *buf = malloc(PAGESIZE());
 
 		instance_ptr = ld->list_ptr[i];
 		fprintf(fp, "instance_ptr = 0x%lx\n", instance_ptr);
@@ -97,7 +113,39 @@ int csdump_metadata(void)
 
 		read_value(cpu, field, coresight_dump_node, cpu);
 		fprintf(fp, "cpu = 0x%x\n", cpu);
+
+		read_value(buf_addr, field, coresight_dump_node, buf);
+		fprintf(fp, "buf_addr = 0x%lx\n", buf_addr);
+
+		read_value(buf_sz, field, coresight_dump_node, buf_size);
+		fprintf(fp, "buf_sz = 0x%x\n", buf_sz);
+
+		read_value(name_addr, field, coresight_dump_node, name);
+		read_string(name_addr, name, 64);
+		fprintf(fp, "name = %s\n", name);
+
+		if (!buf_sz)
+			continue;
+
+		if (!strstr(name, "etm"))
+			continue;
+
+		buf = malloc(buf_sz);
+		readmem(buf_addr, KVADDR, buf, buf_sz, "read page for write",
+			FAULT_ON_ERROR);
+
+		ret = fwrite(buf, buf_sz, 1, out_fp);
+		if (!ret) {
+			fprintf(fp, "[%d] Cannot write file\n", cpu);
+			free(buf);
+			return FALSE;
+		}
+
+		free(buf);
 	}
+
+	fclose(out_fp);
+
 	FREEBUF(ld->list_ptr);
 
 	return 0;
